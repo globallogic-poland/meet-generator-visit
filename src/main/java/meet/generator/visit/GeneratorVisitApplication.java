24f4a31d6cfd2ecb45697ccdd2c2221401e68d01
Robert Gonciarz <robert.gonciarz@globallogic.com>
2018-10-19 15:10:11 +0200

    init

---
+++ src/main/java/meet/generator/visit/GeneratorVisitApplication.java
@@ -0,0 +1,12 @@
+package meet.generator.visit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorVisitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratorVisitApplication.class, args);
+	}
+}
