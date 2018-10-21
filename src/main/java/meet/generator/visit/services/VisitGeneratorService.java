fa1efcbf4f319a73f40708175d3f95c568c68b85
hhh <milosz.mazurek@gmail.com>
2018-10-21 17:14:41 +0200

    Moved desease and drug geeration to visit result generator

--- src/main/java/meet/generator/visit/services/VisitGeneratorService.java
+++ src/main/java/meet/generator/visit/services/VisitGeneratorService.java
@@ -20,8 +20,6 @@ public class VisitGeneratorService {
     private final double[]  doctorsWeights  = {0.1,0.1,0.1,0.5,0.1};
     private final double[]  clinicsWeights  = {0.1,0.2,0.5,0.1,0.1};
     private final double[]  patietsWeights  = {0.1,0.2,0.5,0.1,0.1};
-    //TODO drugs
-    //TODO diseases
 
     public Visit generate() {
         Visit visit = Visit.builder()
@@ -29,8 +27,6 @@ public class VisitGeneratorService {
                 .doctorId(random(doctorsWeights, doctors))
                 .clinicId(random(clinicsWeights, clinics))
                 .patientId(random(patietsWeights, patients))
-                .prescribedDrugs(Arrays.asList(Drug.ABILIFY, Drug.SEROQUEL))
-                .diagnosedDisease(Disease.ADHD)
                 .build();
         return visit;
     }
