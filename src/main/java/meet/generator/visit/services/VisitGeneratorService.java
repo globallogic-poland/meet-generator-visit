2ecbf1b43dc9193babd0b5c771a2fec60d8784bb
Robert Gonciarz <gonciarz@gmail.com>
2018-10-22 22:13:26 +0200

    refactoring

--- src/main/java/meet/generator/visit/services/VisitGeneratorService.java
+++ src/main/java/meet/generator/visit/services/VisitGeneratorService.java
@@ -1,5 +1,7 @@
 package meet.generator.visit.services;
 
+import lombok.AllArgsConstructor;
+import meet.generator.visit.config.GeneratorSettings;
 import meet.generator.visit.model.Visit;
 import org.springframework.stereotype.Service;
 
@@ -8,22 +10,20 @@ import java.util.Random;
 import java.util.UUID;
 
 @Service
+@AllArgsConstructor
 public class VisitGeneratorService {
 
-    //TODO: stream which reads this data from microservices (by Kafka) (and microservices from their casandras)   or   read all to memory and then stream
-    private final static List<String> DOCTORS = List.of("Watson", "Jekyll", "Dolittle", "Ford", "Van Helsing", "House", "Johnson");
-    private final static List<String> CLINICS = List.of("Luxmed", "7", "Staford", "Clinic1", "Clinic2");
-    private final static List<String> PATIENTS = List.of("Mazurek", "Patient1", "Patient2", "Patient3", "Patient4", "Patient4", "Patient4");
-    private final static List<Double> DOCTORS_WEIGHTS = List.of(0.1, 0.1, 0.1, 0.5, 0.1);
-    private final static List<Double> CLINICS_WEIGHTS = List.of(0.1, 0.2, 0.5, 0.1, 0.1);
-    private final static List<Double> PATIENTS_WEIGHTS = List.of(0.1, 0.2, 0.5, 0.1, 0.1);
+    /* TODO: stream which reads this data from microservices (by Kafka)
+    (and microservices from their casandras) or read all to memory and then stream */
+
+    private final GeneratorSettings settings;
 
     public Visit generate() {
         return Visit.builder()
                 .id(UUID.randomUUID().toString())
-                .doctorId(random(DOCTORS_WEIGHTS, DOCTORS))
-                .clinicId(random(CLINICS_WEIGHTS, CLINICS))
-                .patientId(random(PATIENTS_WEIGHTS, PATIENTS))
+                .doctorId(random(settings.getWeights().getDoctors(), settings.getData().getDoctors()))
+                .clinicId(random(settings.getWeights().getClinics(), settings.getData().getClinics()))
+                .patientId(random(settings.getWeights().getPatients(), settings.getData().getPatients()))
                 .build();
     }
 
