23b6ed4eea0e164f28136a11eabac3d97a80c286
--local <--local>
2018-10-22 17:27:48 +0200

    working example - spring cloud streams

--- src/main/java/meet/generator/visit/ports/VisitBindings.java
+++ src/main/java/meet/generator/visit/ports/VisitBindings.java
@@ -1,9 +1,7 @@
 package meet.generator.visit.ports;
 
-import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.SubscribableChannel;
 
 public interface VisitBindings {
 
