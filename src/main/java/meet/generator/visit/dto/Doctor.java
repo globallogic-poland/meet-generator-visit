fd3d56ca029866c4be203dfa2fb154411a28a7b3
Robert Gonciarz <gonciarz@gmail.com>
2018-10-25 00:50:24 +0200

    cleanup

--- src/main/java/meet/generator/visit/dto/Doctor.java
+++ src/main/java/meet/generator/visit/dto/Doctor.java
@@ -1,28 +1,31 @@
 package meet.generator.visit.dto;
 
-import lombok.Builder;
-import lombok.Singular;
-import lombok.Value;
+import lombok.*;
 
 import java.util.Set;
 
-@Value
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+//@Value
 @Builder
 public class Doctor {
 
-    String id;
+    private String id;
 
-    String firstName;
+    private String firstName;
 
-    String lastName;
+    private String lastName;
 
-    String district;
+    private String district;
 
-    String city;
+    private String city;
 
-    String country;
+    private String country;
 
     @Singular
-    Set<Specialization> specializations;
+    private Set<Specialization> specializations;
 
 }
